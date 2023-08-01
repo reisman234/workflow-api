d3d633e9f1782b0598e2c81aabc433d6878c7067
ralph isenmann <ralph.isenmann@hs-offenburg.de>
2023-08-01 16:03:39 +0200

    remove reload from uvicorn

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 # source .venv/bin/activate
 
-uvicorn middlelayer.service_api:service_api --reload --host=0.0.0.0 --port=8080 --root-path=${FASTAPI_ROOT_PATH}
+uvicorn middlelayer.service_api:service_api --host=0.0.0.0 --port=8080 --root-path=${FASTAPI_ROOT_PATH}
